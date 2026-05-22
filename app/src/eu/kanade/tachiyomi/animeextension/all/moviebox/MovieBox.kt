e5637a13032ce936e5057c9f281683dddd2822b4
salmanbappi <salmanbappi691@gmail.com>
2026-05-23 02:37:16 +0600

    fix(moviebox): add missing boolean import to fix compilation error

--- app/src/eu/kanade/tachiyomi/animeextension/all/moviebox/MovieBox.kt
+++ app/src/eu/kanade/tachiyomi/animeextension/all/moviebox/MovieBox.kt
@@ -20,6 +20,7 @@ import eu.kanade.tachiyomi.network.POST
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.JsonElement
 import kotlinx.serialization.json.JsonObject
+import kotlinx.serialization.json.boolean
 import kotlinx.serialization.json.jsonArray
 import kotlinx.serialization.json.jsonObject
 import kotlinx.serialization.json.jsonPrimitive
