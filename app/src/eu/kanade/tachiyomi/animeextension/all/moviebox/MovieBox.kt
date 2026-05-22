e5aa13c61f722c5eb5a99a975e48b20eea91e43a
salmanbappi <salmanbappi691@gmail.com>
2026-05-23 02:53:58 +0600

    fix(moviebox): add missing booleanOrNull import to fix compilation error

--- app/src/eu/kanade/tachiyomi/animeextension/all/moviebox/MovieBox.kt
+++ app/src/eu/kanade/tachiyomi/animeextension/all/moviebox/MovieBox.kt
@@ -22,6 +22,7 @@ import kotlinx.serialization.json.JsonArray
 import kotlinx.serialization.json.JsonElement
 import kotlinx.serialization.json.JsonObject
 import kotlinx.serialization.json.boolean
+import kotlinx.serialization.json.booleanOrNull
 import kotlinx.serialization.json.contentOrNull
 import kotlinx.serialization.json.jsonArray
 import kotlinx.serialization.json.jsonObject
